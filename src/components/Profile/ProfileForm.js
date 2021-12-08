2727e04cd17875d27fa0bfff4b7fd61e3e1cee66
Ajay Kayyam <saiajay220@gmail.com>
2021-12-08 16:15:36 +0530

    Adding passwordChange logic

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,38 @@
-import classes from './ProfileForm.module.css';
-
+import classes from "./ProfileForm.module.css";
+import AuthContext from "../../store/auth-context";
+import { useContext, useRef } from "react";
 const ProfileForm = () => {
+  const authCtx = useContext(AuthContext);
+  const newPasswordRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const newPassword = newPasswordRef.current.value;
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAP7NuBpJkALleoAbUF_zWdIMjoefvLhtI",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: newPassword,
+          retunSecureToken: false,
+        }),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    );
+  };
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={newPasswordRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
